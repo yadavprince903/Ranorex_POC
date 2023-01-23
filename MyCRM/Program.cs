48fee592bdfb7cfadd9e2215aaf85b1ff65a3a94
yadavprince903 <yadavprince903@gmail.com>
2023-01-23 01:10:22 -0800

    my crm

---
+++ MyCRM/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by Ranorex
+ * User: Admin
+ * Date: 1/20/2023
+ * Time: 1:35 AM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+
+using System;
+using System.Threading;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Reporting;
+using Ranorex.Core.Testing;
+
+namespace MyCRM
+{
+    class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            // Uncomment the following 2 lines if you want to automate Windows apps
+            // by starting the test executable directly
+            //if (Util.IsRestartRequiredForWinAppAccess)
+            //    return Util.RestartWithUiAccess();
+
+            Keyboard.AbortKey = System.Windows.Forms.Keys.Pause;
+            int error = 0;
+
+            try
+            {
+                error = TestSuiteRunner.Run(typeof(Program), Environment.CommandLine);
+            }
+            catch (Exception e)
+            {
+                Report.Error("Unexpected exception occurred: " + e.ToString());
+                error = -1;
+            }
+            return error;
+        }
+    }
+}
